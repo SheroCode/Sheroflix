4ed39ffaf8f3b62a7289a9e8ce711dd1177c0a76
Shereen Mohamed <shereen.khalafallah@gmail.com>
2025-05-14 12:45:02 +0300

    Test connection to JSON server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-
+fetch("http://localhost:5000/movies")
+  .then((res) => {
+     console.log(res)
+   return res.json();
+   
+  })
+  .then((data) => {
+    console.log(data)
+    console.log("Monies from server:", data);
+  })
+  .catch((error) => {
+    console.log(("Error fetching movies:", error));
+  });
