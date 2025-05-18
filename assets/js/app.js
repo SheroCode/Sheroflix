1f4f71bd6e5877aa33397b1eac25b064db5d68d4
Shereen Mohamed <shereen.khalafallah@gmail.com>
2025-05-19 00:11:49 +0300

    {

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,12 @@
-const wrapper = document.querySelector(".swiper-wrapper");
+const trendingWrapper = document.querySelector("#trending");
+const movieWrapper = document.querySelector("#movie");
+const tvWrapper = document.querySelector("#tv");
+// overlay variables
+const overlay = document.querySelector(".overlay");
+const detailsImage = document.querySelector(".details__image img");
+const detailsType = document.querySelector(".details__type");
+const detailsDate = document.querySelector(".details__date");
+const detailsDescription = document.querySelector(".details__description");
 const options = {
   method: "GET",
   headers: {
@@ -18,9 +26,9 @@ function initSwiper() {
       prevEl: ".swiper-button-prev",
     },
     breakpoints: {
-      768: { slidesPerView: 2, spaceBetween: 20 },
-      1024: { slidesPerView: 3, spaceBetween: 40 },
-      1440: { slidesPerView: 4, spaceBetween: 50 },
+      768: { slidesPerView: 2, spaceBetween: 10 },
+      1024: { slidesPerView: 4, spaceBetween: 20 },
+      1440: { slidesPerView: 5, spaceBetween: 30 },
     },
   });
 }
@@ -29,16 +37,131 @@ fetch("https://api.themoviedb.org/3/trending/movie/day?language=en-US", options)
   .then((res) => res.json())
   //object destruction
   .then(({ results }) => {
-    displayList(results);
+    displaytrending(results);
     initSwiper();
   })
   .catch((err) => console.error(err));
 
-function displayList(list) {
+function displaytrending(list) {
   list.forEach((ele) => {
     let swiperSlide = document.createElement("div");
+    let img = document.createElement("img");
     swiperSlide.className = "swiper-slide";
-    swiperSlide.innerHTML = `<img  src="https://image.tmdb.org/t/p/original${ele.poster_path}" alt="Movie Poster" />`;
-    wrapper.appendChild(swiperSlide);
+    img.src = `https://image.tmdb.org/t/p/original${ele.poster_path}`;
+    img.alt = "Movie Poster";
+    img.addEventListener("click", function () {
+      showDetails(ele);
+    });
+    swiperSlide.appendChild(img);
+    trendingWrapper.appendChild(swiperSlide);
   });
 }
+// fetch   movies
+fetch(
+  "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+  options
+)
+  .then((res) => res.json())
+  //object destruction
+  .then(({ results }) => {
+    displayMovie(results);
+    initSwiper();
+  })
+  .catch((err) => console.error(err));
+
+function displayMovie(list) {
+  list.forEach((ele) => {
+    let swiperSlide = document.createElement("div");
+    let img = document.createElement("img");
+    swiperSlide.className = "swiper-slide";
+    img.src = `https://image.tmdb.org/t/p/original${ele.poster_path}`;
+    img.alt = "Movie Poster";
+    img.addEventListener("click", function () {
+      showDetails(ele);
+    });
+    swiperSlide.appendChild(img);
+    movieWrapper.appendChild(swiperSlide);
+  });
+}
+
+// fetch tvShows
+fetch("https://api.themoviedb.org/3/tv/popular?language=en-US&page=1", options)
+  .then((res) => res.json())
+  //object destruction
+  .then(({ results }) => {
+    displayTv(results);
+    initSwiper();
+  })
+  .catch((err) => console.error(err));
+function displayTv(list) {
+  list.forEach((ele) => {
+    let swiperSlide = document.createElement("div");
+    let img = document.createElement("img");
+    swiperSlide.className = "swiper-slide";
+    img.src = `https://image.tmdb.org/t/p/original${ele.poster_path}`;
+    img.alt = "TV Show Poster";
+    img.addEventListener("click", function () {
+      showDetails(ele);
+    });
+    swiperSlide.appendChild(img);
+    tvWrapper.appendChild(swiperSlide);
+  });
+}
+
+/*Show details card*/
+
+function showDetails(ele) {
+  overlay.classList.remove("hidden");
+  overlay.classList.add("block");
+  detailsImage.setAttribute(
+    "src",
+    `https://image.tmdb.org/t/p/original${
+      ele.poster_path ? ele.poster_path : ""
+    }`
+  );
+  detailsType.textContent = ele.media_type || (ele.title ? "Movie" : "TV Show");
+  detailsDate.textContent = ele.release_date
+    ? ele.release_date.split("").splice(0, 4).join("")
+    : "2000";
+  detailsDescription.textContent = ele.overview || "No avilabel over view";
+}
+
+/*Close details card*/
+document.querySelector(".close-btn").addEventListener("click", function () {
+  overlay.classList.remove("block");
+  overlay.classList.add("hidden");
+});
+overlay.addEventListener("click", function (e) {
+  // console.log(e.target)
+  if (e.target === overlay) {
+    overlay.classList.remove("block");
+    overlay.classList.add("hidden");
+  }
+});
+
+// function selectChoice(type) {
+//   fetch(
+//     `https://api.themoviedb.org/3/trending/${type}/day?language=en-US`,
+//     options
+//   )
+//     .then((res) => res.json())
+//     //object destruction
+//     .then(({ results }) => {
+//       displayList(results);
+//       initSwiper();
+//     })
+//     .catch((err) => console.error(err));
+// }
+
+// function displayList(list) {
+//   list.forEach((ele) => {
+//     let swiperSlide = document.createElement("div");
+//     swiperSlide.className = "swiper-slide";
+//     swiperSlide.innerHTML = `<img  src="https://image.tmdb.org/t/p/original${ele.poster_path}" alt="Movie Poster" />`;
+//     wrapper.appendChild(swiperSlide);
+//   });
+// }
+// const movie=document.querySelector("#movie").id
+// const tv=document.querySelector("#tv").id
+
+//  selectChoice(tv);
